f028c80978c612034152b4e6e34b9277ed03c268
Max Wihlborg <max.wihlborg@barnebys.com>
2021-12-12 03:21:28 +0100

    day 06 part 2

--- src/06/index.ts
+++ src/06/index.ts
@@ -2,7 +2,7 @@ import F from "lodash/fp";
 
 const parse = F.flow(F.toString, F.trim, F.split(","), F.map(F.toNumber));
 
-function simulate(days: number) {
+function simulateRecursive(days: number) {
   const step = (state: number[], index: number) => {
     if (index <= 0) {
       return state;
@@ -19,14 +19,38 @@ function simulate(days: number) {
   };
 }
 
+function simulateSmart(days: number) {
+  return (state: number[]) => {
+    const map = new Array(9).fill(0);
+    for (const n of state) {
+      map[n] += 1;
+    }
+    for (let i = 0; i < days; i++) {
+      const swp = map.slice(0);
+      for (let j = 0; j < map.length; j++) {
+        if (j > 0) {
+          map[j - 1] += swp[j];
+          map[j] -= swp[j];
+        } else {
+          map[6] += swp[0];
+          map[8] += swp[0];
+          map[0] = 0;
+        }
+      }
+    }
+
+    return F.sum(map);
+  };
+}
+
 export function partOne(input: Buffer) {
-  const answer = F.flow(parse, simulate(80), F.get("length"));
+  const answer = F.flow(parse, simulateRecursive(80), F.get("length"));
 
   return answer(input);
 }
 
 export function partTwo(input: Buffer) {
-  const answer = F.flow(parse, F.constant(0));
+  const answer = F.flow(parse, simulateSmart(256));
 
   return answer(input);
 }
